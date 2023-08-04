42cfb00cb9965aac7b7c122ed79a4ef810a1611a
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2023-08-04 18:05:39 +0200

    added search funcion

--- delete_experiment.py
+++ delete_experiment.py
@@ -6,10 +6,17 @@ message = "You have the below list feel free to delete any of them\n{}:\n".forma
 Entered_name = input(message)
 
 # anonymous function with ternary operator
-def remove_name(name): return True if name != Entered_name else False
+def remove(name): return True if name != Entered_name else False
+
+
+def search(name): return True if name == Entered_name else False
 
 # if you want to delete somthing
-filtered_list = list(filter(remove_name, my_list_of_names))
+filtered_list = list(filter(remove, my_list_of_names))
+searched_name = list(filter(search, my_list_of_names))[0]
 
-print("You deleted the name {} now your list is\n{}".format(
-    Entered_name, filtered_list))
+# print("You deleted the name {} now your list is\n{}".format(
+#     Entered_name, searched_name))
+
+print("You searched for the name '{}' and your search result is\n{}".format(
+    Entered_name, searched_name))
