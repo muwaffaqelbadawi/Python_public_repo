c9e5a52bdf98bc138279dc13eeca263c8f73da0a
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2023-08-05 14:03:41 +0200

    trying class method

---
+++ eg.py
@@ -0,0 +1,32 @@
+
+class Search:
+    # def __init__(self) -> None:
+        
+    entered_name = input("vsdgb")
+    
+    
+    @classmethod
+    def search(name):
+        
+        print(name.entered_name)
+        
+        # return True if name == name.entered_name else False
+
+
+Search.search()
+
+
+
+# class Fruit:
+#     name = 'Fruitas'
+
+#     @classmethod
+#     def printName(cls):
+#         print('The name is:', cls.name)
+
+# Fruit.printName()
+
+
+
+
+
